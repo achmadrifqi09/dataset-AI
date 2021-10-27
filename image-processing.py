de90501a6dc0de48f8a86c6f872db1e13b642fea
najibainun <79824117+najibainun@users.noreply.github.com>
2021-10-27 19:02:33 +0700

    Update image-processing.py

--- image-processing.py
+++ image-processing.py
@@ -11,7 +11,7 @@ files1 = []
 files2 = []
 
 [files1.extend(glob.glob(img_dir + '*.' +e))for e in ext]
-[files2.extend(glob.glob(img_dir2 + '*.' +e))for e int ext]
+[files2.extend(glob.glob(img_dir2 + '*.' +ex))for e int ext]
 
 images1 = [cv2.imread(file1) for file1 in files1]
 images1 = [cv2.imread(file2) for file2 in files2]
@@ -27,7 +27,7 @@ x = 1
 for img2 in images2:
     img_adjusted2 = cv2.addWeighted(img1, 1.5, np.zeros(img2.shape, img2.dtype), 0, 10)
     img_name2 = "dataset/img-proces-kemangi/" + str(x) + ".jpg"
-    cv2.imwrite(img_name2, img_adjusted1)
+    cv2.imwrite(img_name2, img_adjusted2)
     x+=1
         
 
