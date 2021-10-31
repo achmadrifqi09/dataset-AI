b2ff59035d9cdb8387f508f14fe67003710d923d
achmadrifqi09 <79822760+achmadrifqi@users.noreply.github.com>
2021-10-31 21:55:17 +0700

    update
    Menambahkan images processing daun kemangi dan pepaya

--- image-processing.py
+++ image-processing.py
@@ -7,28 +7,33 @@ img_dir2 = "dataset/Kemangi/"
 
 ext = ['jpg', 'png']
 
+
 files1 = []
 files2 = []
 
 [files1.extend(glob.glob(img_dir + '*.' +e))for e in ext]
-[files2.extend(glob.glob(img_dir2 + '*.' +ex))for ex int ext]
+[files2.extend(glob.glob(img_dir2 + '*.' +e))for e in ext]
+
 
 images1 = [cv2.imread(file1) for file1 in files1]
-images1 = [cv2.imread(file2) for file2 in files2]
+images2 = [cv2.imread(file2) for file2 in files2]
 
 i = 1
 for img1 in images1:
     img_adjusted1 = cv2.addWeighted(img1, 1.5, np.zeros(img1.shape, img1.dtype), 0, -12)
-    img_name1 = "dataset/img-proces-pepaya/" + str(i) + ".jpg"
+    img_name1 = "dataset/img-process-pepaya/" + str(i) + ".jpg"
     cv2.imwrite(img_name1, img_adjusted1)
     i+=1
-    
+
 x = 1
 for img2 in images2:
-    img_adjusted2 = cv2.addWeighted(img1, 1.5, np.zeros(img2.shape, img2.dtype), 0, 10)
-    img_name2 = "dataset/img-proces-kemangi/" + str(x) + ".jpg"
+    img_adjusted2 = cv2.addWeighted(img2, 1.5, np.zeros(img2.shape, img2.dtype), 0, 10)
+    img_name2 = "dataset/img-process-kemangi/" + str(x) + ".jpg"
     cv2.imwrite(img_name2, img_adjusted2)
     x+=1
+
+
+
         
 
 
